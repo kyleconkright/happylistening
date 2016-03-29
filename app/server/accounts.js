ad0a9eec4e80ff62d6336b5159a1cc3bbd9a0a3e
kyleconkright <kyleconkright@gmail.com>
2016-03-29 16:02:22 -0700

    renamed songs.js to feed.js

--- app/server/accounts.js
+++ app/server/accounts.js
@@ -4,7 +4,7 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
 	service: "instagram",
-	clientId: "	b64a4afe94a34684bcb7f61c86bc6c4a",
+	clientId: "b64a4afe94a34684bcb7f61c86bc6c4a",
 	scope:'basic',
 	secret: "a2db8632903640f484d2b95941be744d"
 });
