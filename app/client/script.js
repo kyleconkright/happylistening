6db25956ab3d251bf26421f9138c88afdef7c19b
kyleconkright <kyleconkright@gmail.com>
2016-03-30 10:24:39 -0700

    login button added. needs hooked in.

--- app/client/script.js
+++ app/client/script.js
@@ -1,1 +1,31 @@
-alert('hello');
+$(document).ready(() => {
+
+	var login = $('.login');
+	var loginModal = $('#login-modal');
+	var loginModalContent = $('#login-modal div');
+	var close = $('#login-modal .close');
+
+	login.on('click', () => {
+		if(login.hasClass('show')) {
+			login.removeClass('show');
+			loginModal.fadeOut(50, () => {
+				loginModalContent.css({'marginTop': '-10px'});
+			});
+			
+		} else {
+			login.addClass('show');
+			loginModalContent.animate({'marginTop':'0px'}, 350);
+			loginModal.slideDown(100);
+		}
+	});
+
+	close.on('click', () => {
+		if(login.hasClass('show')) {
+			login.removeClass('show');
+			loginModal.fadeOut(50, () => {
+				loginModalContent.css({'marginTop': '-10px'});
+			});
+		}
+	});
+
+}); //jquery
